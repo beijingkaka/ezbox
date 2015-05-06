72a6f11c0227ec7c643c3dfd18b9ff90739fd805
TANG HUI <zetalabs@gmail.com>
2015-05-06 16:42:05 +0800

    sync bootstrap with openwrt trunk 20150506.

--- pool/bootstrap/target/linux/ar71xx/base-files/lib/upgrade/platform.sh
+++ pool/bootstrap/target/linux/ar71xx/base-files/lib/upgrade/platform.sh
@@ -180,10 +180,11 @@ platform_check_image() {
 	ap136-020 | \
 	ap135-020 | \
 	ap96 | \
+	bxu2000n-2-a1 | \
 	db120 | \
 	f9k1115v2 |\
 	hornet-ub | \
-	bxu2000n-2-a1 | \
+	wpj558 | \
 	zcn-1523h-2 | \
 	zcn-1523h-5)
 		[ "$magic_long" != "68737173" -a "$magic_long" != "19852003" ] && {
@@ -223,9 +224,11 @@ platform_check_image() {
 	loco-m-xw | \
 	nanostation-m | \
 	rocket-m | \
+	rocket-m-xw | \
 	nanostation-m-xw | \
 	rw2458n | \
 	wndap360 | \
+	wpj344 | \
 	wzr-hp-g300nh2 | \
 	wzr-hp-g300nh | \
 	wzr-hp-g450h | \
@@ -287,7 +290,8 @@ platform_check_image() {
 	om2p-hs | \
 	om2p-hsv2 | \
 	om2p-lc | \
-	om5p)
+	om5p | \
+	om5p-an)
 		platform_check_image_openmesh "$magic_long" "$1" && return 0
 		return 1
 		;;
@@ -487,7 +491,8 @@ platform_do_upgrade() {
 	om2p-hs | \
 	om2p-hsv2 | \
 	om2p-lc | \
-	om5p)
+	om5p | \
+	om5p-an)
 		platform_do_upgrade_openmesh "$ARGV"
 		;;
 	unifi-outdoor-plus | \
