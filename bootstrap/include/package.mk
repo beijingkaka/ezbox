333ccf1d6c8b13bb05161d339d1187ab617970b5
Zeta Labs <zetalabs@gmail.com>
2015-07-26 14:00:03 +0800

    sync bootstrap with openwrt trunk 20150726.

--- bootstrap/include/package.mk
+++ bootstrap/include/package.mk
@@ -41,42 +41,8 @@ ifdef CONFIG_USE_MIPS16
     TARGET_CFLAGS += -mips16 -minterlink-mips16
   endif
 endif
-ifdef CONFIG_PKG_CHECK_FORMAT_SECURITY
-  ifeq ($(strip $(PKG_CHECK_FORMAT_SECURITY)),1)
-    TARGET_CFLAGS += -Wformat -Werror=format-security
-  endif
-endif
-ifdef CONFIG_PKG_CC_STACKPROTECTOR_REGULAR
-  ifeq ($(strip $(PKG_CC_STACKPROTECTOR_REGULAR)),1)
-    TARGET_CFLAGS += -fstack-protector
-  endif
-endif
-ifdef CONFIG_PKG_CC_STACKPROTECTOR_STRONG
-  ifeq ($(strip $(PKG_CC_STACKPROTECTOR_STRONG)),1)
-    TARGET_CFLAGS += -fstack-protector-strong
-  endif
-endif
-ifdef CONFIG_PKG_FORTIFY_SOURCE_1
-  ifeq ($(strip $(PKG_FORTIFY_SOURCE_1)),1)
-    TARGET_CFLAGS += -D_FORTIFY_SOURCE=1
-  endif
-endif
-ifdef CONFIG_PKG_FORTIFY_SOURCE_2
-  ifeq ($(strip $(PKG_FORTIFY_SOURCE_2)),1)
-    TARGET_CFLAGS += -D_FORTIFY_SOURCE=2
-  endif
-endif
-ifdef CONFIG_PKG_RELRO_PARTIAL
-  ifeq ($(strip $(PKG_RELRO_PARTIAL)),1)
-    TARGET_CFLAGS += -Wl,-z,relro
-  endif
-endif
-ifdef CONFIG_PKG_RELRO_FULL
-  ifeq ($(strip $(PKG_RELRO_FULL)),1)
-    TARGET_CFLAGS += -Wl,-z,now -Wl,-z,relro
-  endif
-endif
 
+include $(INCLUDE_DIR)/hardening.mk
 include $(INCLUDE_DIR)/prereq.mk
 include $(INCLUDE_DIR)/host.mk
 include $(INCLUDE_DIR)/unpack.mk
@@ -105,6 +71,17 @@ STAMP_BUILT:=$(PKG_BUILD_DIR)/.built
 STAMP_INSTALLED:=$(STAGING_DIR)/stamp/.$(PKG_NAME)$(if $(BUILD_VARIANT),.$(BUILD_VARIANT),)_installed
 
 STAGING_FILES_LIST:=$(PKG_NAME)$(if $(BUILD_VARIANT),.$(BUILD_VARIANT),).list
+
+define CleanStaging
+	rm -f $(STAMP_INSTALLED)
+	@-(\
+		cd "$(STAGING_DIR)"; \
+		if [ -f packages/$(STAGING_FILES_LIST) ]; then \
+			cat packages/$(STAGING_FILES_LIST) | xargs -r rm -f 2>/dev/null; \
+		fi; \
+	)
+endef
+
 ifneq ($(if $(CONFIG_SRC_TREE_OVERRIDE),$(wildcard ./git-src)),)
   USE_GIT_TREE:=1
   QUILT:=1
@@ -203,6 +180,7 @@ define Build/DefaultTargets
 
   $(call Build/Exports,$(STAMP_CONFIGURED))
   $(STAMP_CONFIGURED): $(STAMP_PREPARED)
+	$(CleanStaging)
 	$(foreach hook,$(Hooks/Configure/Pre),$(call $(hook))$(sep))
 	$(Build/Configure)
 	$(foreach hook,$(Hooks/Configure/Post),$(call $(hook))$(sep))
@@ -220,7 +198,6 @@ define Build/DefaultTargets
 
   $(STAMP_INSTALLED) : export PATH=$$(TARGET_PATH_PKG)
   $(STAMP_INSTALLED): $(STAMP_BUILT)
-	$(SUBMAKE) -j1 clean-staging
 	rm -rf $(TMP_DIR)/stage-$(PKG_NAME)
 	mkdir -p $(TMP_DIR)/stage-$(PKG_NAME)/host $(STAGING_DIR)/packages $(STAGING_DIR_HOST)/packages
 	$(foreach hook,$(Hooks/InstallDev/Pre),\
@@ -331,16 +308,9 @@ prepare:
 configure:
 compile: prepare-package-install
 install: compile
-clean-staging: FORCE
-	rm -f $(STAMP_INSTALLED)
-	@-(\
-		cd "$(STAGING_DIR)"; \
-		if [ -f packages/$(STAGING_FILES_LIST) ]; then \
-			cat packages/$(STAGING_FILES_LIST) | xargs -r rm -f 2>/dev/null; \
-		fi; \
-	)
 
-clean: clean-staging FORCE
+clean: FORCE
+	$(CleanStaging)
 	$(call Build/UninstallDev,$(STAGING_DIR),$(STAGING_DIR_HOST))
 	$(Build/Clean)
 	rm -f $(STAGING_DIR)/packages/$(STAGING_FILES_LIST) $(STAGING_DIR_HOST)/packages/$(STAGING_FILES_LIST)
